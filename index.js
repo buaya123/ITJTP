9b107be5c3ffe75e387febac0df7b961780f4d0f
keejay70 <setenta.kent@gmail.com>
2021-06-05 05:19:11 +0800

    Login and Register Views

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
 })
 
 app.listen(port, () => {
